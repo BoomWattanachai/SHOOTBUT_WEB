544b5ae41bb4ee3c46b86611ea08f70db5376cbd
wattanachaiBoom <boom_sirisuk@hotmail.com>
2020-03-11 11:01:00 +0700

    create project

---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app dark>
+    <v-app-bar :clipped-left="clipped" fixed app>
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+    </v-app-bar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-footer :fixed="fixed" app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Shoot buy'
+    }
+  }
+}
+</script>
