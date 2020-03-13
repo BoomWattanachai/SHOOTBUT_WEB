3ff8b2a470fc65ebf4b18fffa3c3b2e97d063ec7
wattanachaiBoom <boom_sirisuk@hotmail.com>
2020-03-13 16:26:22 +0700

    merge pik and bug search,detailData

--- pages/testTest.vue
+++ pages/testTest.vue
@@ -27,8 +27,8 @@
 
     <v-row justify="center">
       <!-- <v-expansion-panels v-for="(message, i) in filteredMessage" :key="i"> -->
-      <v-expansion-panels v-for="(message, i) in filteredMessage" :key="i">
-        <v-expansion-panel>
+      <v-expansion-panels v-for="(message, i) in filteredMessage2" :key="i">
+        <v-expansion-panel v-for="(order, j) in message.order" :key="j">
           <v-expansion-panel-header>
             <v-row align="center" class="spacer" no-gutters>
               <v-col cols="4" sm="2" md="1">
@@ -36,7 +36,7 @@
               </v-col>
 
               <v-col class="hidden-xs-only" sm="5" md="3">
-                <h3 v-html="message.orderId"></h3>
+                <h3 v-html="order.orderId"></h3>
               </v-col>
 
               <v-col class="text-no-wrap" cols="5" sm="2">
@@ -63,7 +63,13 @@
           <v-expansion-panel-content>
             <v-divider></v-divider>
             <v-card-text v-text="lorem"></v-card-text>
-            <h3>{{ message.orderId }}</h3>
+            <h3
+              v-if(this.messages)
+              v-for="(orderDetail, index) in order.orderDetail"
+              :key="index"
+            >
+              {{ orderDetail.product.productDetail }}
+            </h3>
           </v-expansion-panel-content>
         </v-expansion-panel>
       </v-expansion-panels>
@@ -72,35 +78,124 @@
 </template>
 <script lang="ts">
 import Vue from 'vue'
-import * as aaa from '@/store/authenModule/apiService'
+import * as apiService from '@/store/authenModule/apiService'
 
 export default Vue.extend({
   data() {
     return {
+      //   itemsPerPageArray: [4, 8, 12],
       messages: [],
+      dataList: [],
       lorem: 'aaaaaaa',
       searchByOrderNumber: '',
       searchByUserEmail: '',
-      order: []
-    }
-  },
-  computed: {
-    filteredMessage() {
-      return this.messages.filter((message) => {
-        return (
-          message.email.toLowerCase().match(this.searchByUserEmail) &&
-          message.orderId.toString().match(this.searchByOrderNumber)
-        )
-      })
+      order: [],
+      isLoaded: false
     }
   },
   async created() {
-    await aaa.getUserList().then((res: any) => {
-      this.messages = res
-
-      console.log(res)
+    await apiService.getUserList().then((data) => {
+      const userDataList = data
+      userDataList.forEach((userData: any) => {
+        userData.order.forEach((order: any) => {
+          order.orderDetail.forEach(async (orderDetail: any) => {
+            const product = orderDetail.product
+            if (product.categoryId === 1) {
+              await apiService
+                .selectProductFoodData(product.productId)
+                .then((response) => {
+                  const data = response[0]
+                  orderDetail.product.productDetail = {
+                    image: data.foodAndBevImage,
+                    brand: data.foodAndBevBrand,
+                    model: data.foodAndBevModel,
+                    price: data.foodAndBevPrice
+                  }
+                })
+            } else if (product.categoryId === 2) {
+              await apiService
+                .selectProductFoodData(product.productId)
+                .then((response) => {
+                  const data = response[0]
+                  orderDetail.product.productDetail = {
+                    image: data.electronicImage,
+                    brand: data.electronicBrand,
+                    model: data.electronicModel,
+                    price: data.electronicPrice
+                  }
+                })
+            } else if (product.categoryId === 3) {
+              await apiService
+                .selectProductFoodData(product.productId)
+                .then((response) => {
+                  const data = response[0]
+                  orderDetail.product.productDetail = {
+                    image: data.furnitureImage,
+                    brand: data.furnitureBrand,
+                    model: data.furnitureModel,
+                    price: data.furniturePrice
+                  }
+                })
+            }
+          })
+        })
+      })
+      this.dataList = userDataList
+      this.messages = userDataList
+      console.log(userDataList)
+      this.isLoaded = true
+      // userData[0].order[0].orderDetail[0].product.productDetail = this.testObject
+      // console.log(userData[0].order[0].orderDetail[0].product)
     })
   },
+  computed: {
+    filteredMessage2() {
+      if (!this.isLoaded) return null
+      // const result = this.messages.filter((message) => {
+      //   return message.order.find((order) => {
+      //     // return order
+      //     // console.log('orderId' + order)
+      //     let a: null
+      //     if (
+      //       (a = order.orderId
+      //         .toString()
+      //         .match(this.searchByOrderNumber.toString())) &&
+      //       (a = message.email
+      //         .toLowerCase()
+      //         .match(this.searchByUserEmail.toLowerCase()))
+      //     )
+      //       return a
+      //   })
+      // })
+      // return result
+      let listMessage = null
+      listMessage = this.dataList
+      const result = listMessage.filter((message) => {
+        message.order = message.order.filter((order) => {
+          // return order.orderId
+          //   .toString()
+          //   .match(this.searchByOrderNumber.toString())
+          // console.log(order)
+          // return order.find((orderId) => {
+          let a: null
+          if (
+            (a = order.orderId
+              .toString()
+              .match(this.searchByOrderNumber.toString())) &&
+            (a = message.email
+              .toLowerCase()
+              .match(this.searchByUserEmail.toLowerCase()))
+          )
+            return a
+          // })
+        })
+        return message.order
+      })
+      console.log('Done')
+      return result
+    }
+  },
+
   methods: {}
 })
 </script>
